6a5fb81d8c0ef25749c91016d7d27adc295570f7
Tex-Tang <tex_tang@outlook.com>
2019-12-17 12:55:07 +0800

    Update frontend

---
+++ client/src/layouts/entry.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="entry-layout">
+    <div class="action-bar">
+      <i class="icon icon-back"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.action-bar{
+}
+</style>
---
+++ client/src/pages/entry/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    data: {
+      groupName: "",
+      members: [
+        { name: "", id: "" },
+        { name: "", id: "" },
+        { name: "", id: "" },
+        { name: "", id: "" }
+      ]
+    }
+  })
+}
+</script>
+
+<style>
+
+</style>
