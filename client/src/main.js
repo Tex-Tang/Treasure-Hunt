8d34698973986a1481e4bf7e54c7b5ecb52116fa
Tex-Tang <tex_tang@outlook.com>
2019-12-17 13:37:05 +0800

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import './assets/scss/spectre/spectre.scss'
+import './assets/scss/spectre/spectre-exp.scss'
+import './assets/scss/spectre/spectre-icons.scss'
+
 new Vue({
   router,
   render: h => h(App)
