169882b1685df3accc3861a6f057267b19bff070
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2019-12-18 16:41:01 +0800

    commit

--- client/src/pages/game/question/list.vue
+++ client/src/pages/game/question/list.vue
@@ -1,5 +1,29 @@
 <template>
-  
+    <div>
+        <div class="container">
+            <i class="icon icon-menu col-1" w></i>
+            <div class="title col-mx-auto">Questions</div>
+            <i class="icon icon-location col-1"></i>
+        </div>
+        <br>
+        <div class="list">
+            <div class="flex-container">
+                <button class="btn">Question1</button>
+                <button class="btn">Question2</button>
+            </div>
+            <div class="flex-container">
+                <button class="btn">Question3</button>
+                <button class="btn">Question4</button>
+            </div>
+        </div>
+        <br>
+        <div class="score">
+            <h1>TeamName</h1>
+            <h1>Score</h1>
+        </div>
+    </div>
+    
+    
 </template>
 
 <script>
@@ -8,6 +32,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
 
-</style>
+.flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.left {
+    justify-content: left;
+    align-items: center;
+}
+
+.title {
+    justify-content: center;
+    align-items: center;
+}
+
+.right {
+    justify-content: right;
+    align-items: center;
+}
+
+.list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center
+}
+
+.btn {
+    width: 200px;
+    height: 200px;
+}
+</style>>
--- client/src/pages/game/question/single.vue
+++ client/src/pages/game/question/single.vue
@@ -1,5 +1,19 @@
 <template>
-  
+  <div>
+        <div class="container">
+            <i class="icon icon-menu col-1" w></i>
+            <div class="title col-mx-auto">Questions</div>
+            <i class="icon icon-location col-1"></i>
+        </div>
+        <br>
+        <div class="container">
+            <form class="form">
+                <h1>Question</h1>
+                <input type="text" value="answer">
+                <button type="submit" class="btn">Submit</button>
+            </form>
+        </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +22,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
 
+.form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+}
 </style>
