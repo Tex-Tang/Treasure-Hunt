62b486b9c92464a1ac12ff5c1243307583c04af2
KenjiPcx <56083944+KenjiPcx@users.noreply.github.com>
2019-12-18 20:14:40 +0800

    cant align the score to vertical bottom

--- client/src/pages/game/question/list.vue
+++ client/src/pages/game/question/list.vue
@@ -1,29 +1,27 @@
 <template>
-    <div>
+    <div class="big-container">
         <div class="container">
-            <i class="icon icon-menu col-1" w></i>
-            <div class="title col-mx-auto">Questions</div>
-            <i class="icon icon-location col-1"></i>
+            <i class="icon icon-menu" w></i>
+            <div class="title">Questions</div>
+            <i class="icon icon-location"></i>
         </div>
         <br>
-        <div class="list">
-            <div class="flex-container">
-                <button class="btn">Question1</button>
-                <button class="btn">Question2</button>
+        <div>
+            <div class="columns col-lg-auto">
+                <button class="btn column col-lg-auto">Question1</button>
+                <button class="btn column col-lg-auto">Question2</button>
             </div>
-            <div class="flex-container">
-                <button class="btn">Question3</button>
-                <button class="btn">Question4</button>
+            <div class="columns col-lg-auto">
+                <button class="btn column col-lg-auto">Question3</button>
+                <button class="btn column col-lg-auto">Question4</button>
             </div>
         </div>
         <br>
-        <div class="score">
-            <h1>TeamName</h1>
-            <h1>Score</h1>
+        <div class="container bot">
+            <div>TeamName</div>
+            <div>Score</div>
         </div>
     </div>
-    
-    
 </template>
 
 <script>
@@ -33,44 +31,39 @@ export default {
 </script>
 
 <style scoped>
+.big-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
 .container {
+    width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+    padding: 25px;
 }
 
 .flex-container {
+    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
 }
 
-.left {
-    justify-content: left;
-    align-items: center;
-}
-
-.title {
+.columns {
+    align-content: center;
     justify-content: center;
-    align-items: center;
-}
-
-.right {
-    justify-content: right;
-    align-items: center;
-}
-
-.list {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center
 }
 
 .btn {
-    width: 200px;
+    width: 50%;
     height: 200px;
+}
+
+.bot {
+    vertical-align: bottom;
 }
 </style>>
