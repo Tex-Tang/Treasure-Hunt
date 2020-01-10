f97c156be1f97941c0481f0cfafa2ab9bd609210
Kennyhow <kennethyonghw@gmail.com>
2020-01-11 00:31:42 +0800

    create MmhMiddleware

---
+++ backend/app/Http/Middleware/MmhMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class MmhMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+        if (Auth::user()->went_to_mmh_status == true) {
+            return $next($request);
+        }
+
+        return redirect('/please_dont_do_this_to_us_thanks');
+    }
+}
